78e0b3b99a48c178d196366796216ae6ac54f32e
Diego Dubois <ddubois1780@gmail.com>
2025-01-02 12:12:43 +0100

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './style.scss'
 import Header from './components/Header.jsx'
 import HeroHome from './components/HeroHome.jsx'
 import Grid from './components/grid.jsx'
+import Footer from "./components/footer.jsx";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Header />
     <HeroHome />
     <Grid />
+    <Footer />
     </>
   )
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <footer className='footer'>
+        <div className="footer-content">
+            <img className='img-footer' src="../public/footerlogo.svg" alt="Logo Kaza" />
+            <p className="copyright">© 2020 Kasa. All rights reserved</p>
+        </div>
+    </footer>
+  )
+}
+
+export default footer
