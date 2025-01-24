7d7951625088ae5073721494b66ade5c5ce85c53
Diego Dubois <ddubois1780@gmail.com>
2025-01-24 12:30:10 +0100

    Correction Soutenance

--- src/pages/Accommodation.jsx
+++ src/pages/Accommodation.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import AccomodationDescription from "../components/AccomodationDescription";
-import { useParams } from "react-router";
 
 const Accommodation = () => {
   return (
